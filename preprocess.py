a79fcdf3c6468e14439acd7ce0b2204fd5a9a164
cschaefer26 <c.schaefer.home@gmail.com>
2020-05-11 14:44:03 +0200

    Changed preprocessing to librosa.

--- preprocess.py
+++ preprocess.py
@@ -5,6 +5,7 @@ import torch
 import argparse
 import numpy as np
 
+from utils.dsp import melspectrogram, load_wav
 from utils.stft import TacotronSTFT
 from utils.hparams import HParam
 from utils.utils import read_wav_np
@@ -22,17 +23,15 @@ def main(hp, args):
     wav_files = glob.glob(os.path.join(args.data_path, '**', '*.wav'), recursive=True)
 
     for wavpath in tqdm.tqdm(wav_files, desc='preprocess wav to mel'):
-        sr, wav = read_wav_np(wavpath)
-        assert sr == hp.audio.sampling_rate, \
-            "sample rate mismatch. expected %d, got %d at %s" % \
-            (hp.audio.sampling_rate, sr, wavpath)
-        
+        wav = load_wav(wavpath)
         if len(wav) < hp.audio.segment_length + hp.audio.pad_short:
             wav = np.pad(wav, (0, hp.audio.segment_length + hp.audio.pad_short - len(wav)), \
                     mode='constant', constant_values=0.0)
 
-        wav = torch.from_numpy(wav).unsqueeze(0)
-        mel = stft.mel_spectrogram(wav)
+        mel = melspectrogram(wav)
+        mel = torch.tensor(mel).unsqueeze(0).float()
+        #wav = torch.from_numpy(wav).unsqueeze(0)
+        #mel = stft.mel_spectrogram(wav)
 
         melpath = wavpath.replace('.wav', '.mel')
         torch.save(mel, melpath)
---
+++ utils/dsp.py
@@ -0,0 +1,70 @@
+import math
+import torch
+import numpy as np
+import librosa
+from scipy.signal import lfilter
+
+class hp:
+    n_fft = 2048
+    num_mels = 80
+    sample_rate = 22050
+    fmin = 40
+    min_level_db = -100
+    ref_level_db = 20
+    hop_length = 256
+    win_length = 1024
+
+
+def load_wav(path):
+    return librosa.load(path, sr=hp.sample_rate)[0]
+
+
+def save_wav(x, path):
+    librosa.output.write_wav(path, x.astype(np.float32), sr=hp.sample_rate)
+
+
+def linear_to_mel(spectrogram):
+    return librosa.feature.melspectrogram(
+        S=spectrogram, sr=hp.sample_rate, n_fft=hp.n_fft, n_mels=hp.num_mels, fmin=hp.fmin)
+
+'''
+def build_mel_basis():
+    return librosa.filters.mel(hp.sample_rate, hp.n_fft, n_mels=hp.num_mels, fmin=hp.fmin)
+'''
+
+def normalize(S):
+    return np.clip((S - hp.min_level_db) / -hp.min_level_db, 0, 1)
+
+
+def denormalize(S):
+    return (np.clip(S, 0, 1) * -hp.min_level_db) + hp.min_level_db
+
+
+def amp_to_db(x):
+    return 20 * np.log10(np.maximum(1e-5, x))
+
+
+def db_to_amp(x):
+    return np.power(10.0, x * 0.05)
+
+
+def spectrogram(y):
+    D = stft(y)
+    S = amp_to_db(np.abs(D)) - hp.ref_level_db
+    return normalize(S)
+
+
+def melspectrogram(y):
+    D = stft(y)
+    S = amp_to_db(linear_to_mel(np.abs(D)))
+    return normalize(S)
+
+def raw_melspec(y):
+    D = stft(y)
+    S = linear_to_mel(np.abs(D))
+    return S
+
+def stft(y):
+    return librosa.stft(
+        y=y,
+        n_fft=hp.n_fft, hop_length=hp.hop_length, win_length=hp.win_length)
